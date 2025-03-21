caca9c927beae9cf454229ded9b86387528f9ec7
Noora Panahi <nourap064@gmail.com>
2025-03-21 08:11:58 -0700

    added two new functions to change characters

--- GameCharacter/game.hpp
+++ GameCharacter/game.hpp
@@ -9,6 +9,8 @@ class Game{
         std::vector<Character> characters ; 
     public:
         void addCharacters(const Character& character) ; 
+        void updateCharacter(const std::string& Name, const std::string& attribute, int value);
+        void updateCharacter(const std::string& Name, const std::string& attribute, const std::string& value);
         void displayCharacters()const ;
 };
 
