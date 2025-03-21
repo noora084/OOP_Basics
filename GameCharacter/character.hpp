5995a5f7f6f21e12491c3eed7eb777b774df03b2
Noora Panahi <nourap064@gmail.com>
2025-03-21 08:13:32 -0700

    added two functions called 'update'

--- GameCharacter/character.hpp
+++ GameCharacter/character.hpp
@@ -20,6 +20,8 @@ class Character{
         Character(const std::string &name , const std::string &power , int health) ; 
         void addSkill(const std::string& skillName, int level);
         void showCharacters()const ; 
+        void update(const std::string & attribute , int value) ; 
+        void update(const std::string& attribute, const std::string & value) ; 
         std::string getName()const ; 
         int getHealth()const ; 
         void setHealth(int newHealth);
