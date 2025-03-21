49f78a0d7fcfad074636a54dd69fca3b626f1989
Noora Panahi <nourap064@gmail.com>
2025-03-21 08:07:54 -0700

    now user can change some details about characters

--- GameCharacter/game.cpp
+++ GameCharacter/game.cpp
@@ -6,7 +6,24 @@ using namespace std ;
 void Game::addCharacters(const Character& character){
     characters.push_back(character) ; 
 }
-
+void Game::updateCharacter(const string& Name, const string& attribute, int value){
+    for(auto& chara : characters){
+        if(chara.getName() == Name){
+            chara.update(attribute , value) ;
+            return; 
+        }
+    }
+    cout << "Character with name \"" << Name << "\" not found.\n";
+}
+void Game::updateCharacter(const string& Name, const string& attribute, const string& value){
+    for(auto& chara : characters){
+        if(chara.getName() == Name){
+            chara.update(attribute , value) ; 
+            return ; 
+        }
+    }
+    cout << "Character with name \"" << Name << "\" not found.\n";
+}
 void Game::displayCharacters() const{
     cout << "Game Characters:" << endl;
     for (const auto& character : characters) {
