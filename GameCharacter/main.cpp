b9b99bc1ac6e07418a6bc6f87fd1dfcb9b46881a
Noora Panahi <nourap064@gmail.com>
2025-03-21 01:35:08 -0700

    defined two characters, added their skills and displayed them

---
+++ GameCharacter/main.cpp
@@ -0,0 +1,17 @@
+#include "game.hpp"
+#include <iostream>
+
+int main(){
+    Character warrior("warrior" , "swordplay" , 10) ; 
+    warrior.addSkill("sword fighting" , 12) ; 
+    warrior.addSkill("Defense", 8);
+
+    Character roya("roya", "defence", 60);
+    roya.addSkill("Stealth", 8);
+
+    Game game;
+    game.addCharacters(warrior);
+    game.addCharacters(roya);
+
+    game.displayCharacters();
+}
