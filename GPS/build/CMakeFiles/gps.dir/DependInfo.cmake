1498b9d263d80fd57a95baa3914dd0f3225ba182
Noora Panahi <nourap064@gmail.com>
2025-03-21 02:00:39 -0700

    CMake folder

---
+++ GPS/build/CMakeFiles/gps.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/sibegilan/Desktop/HW2/GPS/gps.cpp" "CMakeFiles/gps.dir/gps.cpp.obj" "gcc" "CMakeFiles/gps.dir/gps.cpp.obj.d"
+  "C:/Users/sibegilan/Desktop/HW2/GPS/main.cpp" "CMakeFiles/gps.dir/main.cpp.obj" "gcc" "CMakeFiles/gps.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GPS/build/CMakeFiles/gps.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gps.dir/gps.cpp.obj"
+  "CMakeFiles/gps.dir/gps.cpp.obj.d"
+  "CMakeFiles/gps.dir/main.cpp.obj"
+  "CMakeFiles/gps.dir/main.cpp.obj.d"
+  "gps.exe"
+  "gps.exe.manifest"
+  "gps.pdb"
+  "libgps.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
