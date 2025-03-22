2495fdb89486bd72d717c1f5e082e1f7a126bcdc
Noora Panahi <nourap064@gmail.com>
2025-03-22 12:02:59 -0700

    now events have startTime and endTime

--- CalendarEvent/main.cpp
+++ CalendarEvent/main.cpp
@@ -1,17 +1,17 @@
 #include "calender.hpp"
 #include "event.hpp"
-
 #include <iostream>
 #include <ctime>
 
 using namespace std ; 
 
 int main(){
-    Event event1("meeting" , 1970  , 1 , 21) ; 
-    Event event2("lunch" , 2025  , 5 , 40) ; 
-    Event event3("dinner" , 2037 , 4 , 3) ; 
+    Event event1("meeting" , 2026  , 1 , 21 , 2027 , 2 , 3) ; 
+    Event event2("lunch" , 2025 , 5 , 40 , 2060 , 6 , 21 ) ; 
+    Event event3("dinner" , 2037 , 4 , 3 , 2021 , 4 , 8) ; 
 
     Calender cal ; 
+    
     cal.addEvent(event1) ; 
     cal.addEvent(event2) ;
     cal.addEvent(event3) ; 
