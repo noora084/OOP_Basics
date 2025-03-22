a1dc46a1b395a2c1280e5b3ef24975d5a75426be
Noora Panahi <nourap064@gmail.com>
2025-03-22 12:05:06 -0700

    defined start time and end time for events

--- CalendarEvent/event.hpp
+++ CalendarEvent/event.hpp
@@ -5,17 +5,27 @@
 #include <iostream>
 #include <ctime>
 
+struct start_time{
+    int year ; 
+    int month ;
+    int day ;  
+};
+
+struct end_time{
+    int year ; 
+    int month ;
+    int day ;  
+};
 
 class Event{
     public: 
         std::string name ;
-        std::time_t year ; 
-        std::time_t month ;
-        std::time_t day ; 
+        start_time start ; 
+        end_time end ;
         bool isdeleted ; 
         void setDeleted() ; 
         bool getDeleted() const ; 
-        Event(std::string name, std::time_t year , std::time_t month , std::time_t day);
+        Event(std::string name, int startYear , int startMonth , int startDay , int endYear , int endMonth , int endDay);
         ~Event() ; 
 };
 
