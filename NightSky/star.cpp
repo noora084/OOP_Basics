935d6cc1b61dd042f21e86ed1a4cd4687ad5277b
noora084 <nourap064@gmail.com>
2025-03-26 19:18:51 +0330

    changed the validation

--- NightSky/star.cpp
+++ NightSky/star.cpp
@@ -4,7 +4,7 @@ using namespace std ;
 
 Star::Star(int x , int y , int brightness , std::string color):
             x(x) , y(y) , brightness(brightness) , color(color){
-            if (x < 0 || x > 50 || y < 0 || y > 20) { // imaginary sky
+            if (x < 0 || x > 400 || y < 0 || y > 400) { // imaginary sky
                 throw out_of_range("Star coordinates out of sky bounds!");
             }
 }
@@ -13,4 +13,7 @@ int Star::getX()const{
 }
 int Star::getY()const{
     return y ; 
+}
+int Star::getBrightness()const{
+    return brightness ; 
 }
