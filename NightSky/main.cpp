291093c7d23a2d764f4777857020eb3a6168fdcd
noora084 <nourap064@gmail.com>
2025-03-26 19:18:05 +0330

    adding more stars

--- NightSky/main.cpp
+++ NightSky/main.cpp
@@ -3,16 +3,22 @@
 #include <stdexcept>
 #include <iostream>
 using namespace std ; 
+
 int main(){
     try{
         Sky nightSky;
-        Star Star1(10, 5, 3, "yellow") ;
-        Star Star2(20, 10, 2, "white") ; 
-        Star Star3(49, 15, 1, "blue") ; 
+        Star Star1(300, 55, 3, "yellow") ;
+        Star Star2(199, 109, 2, "white") ; 
+        Star Star3(350, 109, 1, "blue") ; 
+        Star Star4(125, 299, 20, "blue") ; 
+
     
         nightSky.addStars(Star1);
         nightSky.addStars(Star2);
         nightSky.addStars(Star3);
+        nightSky.addStars(Star4);
+
+
     
         nightSky.displaysky();
     }
